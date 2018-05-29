ae7f33d2b15c218234459ecd186f88878291f346
Felix Lösing <felix.loesing@me.com>
2018-05-29 23:04:07 +0200

    project clean-up

---
+++ Placestar/Location.swift
@@ -0,0 +1,87 @@
+//
+//  Location.swift
+//  Placestar
+//
+//  Created by Felix Lösing on 14.09.15.
+//  Copyright (c) 2015 Felix Lösing. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import MapKit
+
+class Location: NSManagedObject, MKAnnotation {
+
+    /*
+    @NSManaged var latitude: Double
+    @NSManaged var longitude: Double
+    @NSManaged var date: NSDate
+    @NSManaged var locationDescription: String
+    @NSManaged var category: String
+    @NSManaged var placemark: CLPlacemark?
+    */
+    
+    var coordinate: CLLocationCoordinate2D {
+        return CLLocationCoordinate2DMake(latitude, longitude)
+    }
+    
+    var title: String? {
+        if locationDescription.isEmpty {
+            return NSLocalizedString("(no-description)", value: "(No Description)", comment: "")
+        } else {
+            return locationDescription
+        
+    }
+    }
+    
+    var subtitle: String? {
+        return category
+    }
+    
+    var photoPath: String {
+        assert(photoID != nil, "No photo ID set")
+        let filename = "Photo-\(photoID!.intValue).jpg"
+        return (applicationDocumentsDirectory as NSString).appendingPathComponent(filename)
+    }
+    
+    var photoImage: UIImage? {
+        return UIImage(contentsOfFile: photoPath)
+    }
+    
+    
+    
+    class func nextPhotoID() -> Int {
+        let userDefaults = UserDefaults.standard
+        let currentID = userDefaults.integer(forKey: "PhotoID")
+        userDefaults.set(currentID + 1, forKey: "PhotoID")
+        userDefaults.synchronize()
+        return currentID
+        
+    }
+    
+    var hasPhoto: Bool {
+        return photoID != nil
+    }
+    
+    
+    func removePhotoFile() {
+        if hasPhoto {
+            let path = photoPath
+            
+            let fileManager = FileManager.default
+            if fileManager.fileExists(atPath: path) {
+                do {
+                    try fileManager.removeItem(atPath: path)
+                } catch {
+                    print("*** Error removing file \(error)")
+                }
+            }
+        }
+    }
+
+
+}
+
+
+
+
---
+++ Placestar/LocationCell.swift
@@ -0,0 +1,67 @@
+//
+//  LocationCell.swift
+//  Placestar
+//
+//  Created by Felix Lösing on 06.03.16.
+//  Copyright © 2016 Felix Lösing. All rights reserved.
+//
+
+import UIKit
+
+class LocationCell: UITableViewCell {
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        photoImageView.layer.cornerRadius = photoImageView.bounds.size.width / 2
+        photoImageView.clipsToBounds = true
+        separatorInset = UIEdgeInsets(top: 0, left: 82, bottom: 0, right: 0)
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func configureForLocation(_ location: Location) {
+        
+        if location.locationDescription.isEmpty {
+            descriptionLabel.text = NSLocalizedString("(no-description)", value: "(No Description)", comment: "")
+        } else {
+            descriptionLabel.text = location.locationDescription
+        }
+        
+        if let placemark = location.placemark {
+            var text = ""
+            if let s = placemark.subThoroughfare {
+                text += s + " "
+            }
+            if let s = placemark.thoroughfare {
+                text += s + ", "
+            }
+            if let s = placemark.locality {
+                text += s
+            }
+            addressLabel.text = text
+        } else {
+            addressLabel.text = String(format: "Lat: %.8f, Long. %.8f", location.latitude, location.longitude)
+        }
+        
+        photoImageView.image = imageForLocation(location)
+        
+    }
+    
+    func imageForLocation(_ location: Location) -> UIImage {
+        if location.hasPhoto, let image = location.photoImage {
+            return image
+            //.resizedImageWithBounds(CGSize(width: 52, height: 52))
+        }
+        return UIImage(named: "NoPhoto")!
+    }
+
+}
